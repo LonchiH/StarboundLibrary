4ecece57cfbb0fa8d4a09d29027be96d18d5944c
Lancelot <bulanlancelot@gmail.com>
2023-11-16 00:11:55 +0800

    implement initial splash

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 
 android {
     namespace = "com.example.starboundlibrary"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.starboundlibrary"
-        minSdk = 24
+        minSdk = 31
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
@@ -44,4 +44,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Splash API
+    implementation("androidx.core:core-splashscreen:1.0.0")
 }
