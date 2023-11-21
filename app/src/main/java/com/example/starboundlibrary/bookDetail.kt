c2cf9e96e8fc199445a184a58b1b16fa40eb5dc8
misky3 <33999758+misky3@users.noreply.github.com>
2023-11-21 22:13:44 +0800

    finished the ui I think

---
+++ app/src/main/java/com/example/starboundlibrary/bookDetail.kt
@@ -0,0 +1,11 @@
+package com.example.starboundlibrary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class bookDetail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_book_detail)
+    }
+}
