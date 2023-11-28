3cbc2510821070e2ea9ed69051907634c63ffa5b
Lancelot <bulanlancelot@gmail.com>
2023-11-29 01:52:52 +0800

    add other firebase dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
     // Add the Google services Gradle plugin
     id("com.google.gms.google-services")
+    id("com.google.firebase.crashlytics")
 
 }
 
@@ -49,6 +50,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-crashlytics:18.6.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -61,12 +63,15 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
     // When using the BoM, don't specify versions in Firebase dependencies
     implementation("com.google.firebase:firebase-analytics")
-    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-auth:22.3.0")
     implementation("com.google.firebase:firebase-database")
 
     // for adding recyclerview
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     // for adding cardview
     implementation("androidx.cardview:cardview:1.0.0")
+
+    // google analytics
+    implementation("com.google.firebase:firebase-analytics:21.5.0")
 
 }
