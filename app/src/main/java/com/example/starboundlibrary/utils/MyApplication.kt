1ab9784d9e73c85b0eea99630a10c7621b1fd0fe
Lancelot <bulanlancelot@gmail.com>
2023-12-12 15:21:12 +0800

    Added load category and pdfsize

--- app/src/main/java/com/example/starboundlibrary/utils/MyApplication.kt
+++ app/src/main/java/com/example/starboundlibrary/utils/MyApplication.kt
@@ -8,7 +8,12 @@ import android.widget.ProgressBar
 import android.widget.TextView
 import com.example.starboundlibrary.utils.Constants
 import com.github.barteksc.pdfviewer.PDFView
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.storageMetadata
 import java.util.Calendar
 import java.util.Locale
 
@@ -25,18 +30,26 @@ class MyApplication : Application() {
             return DateFormat.format("dd/MM/yyyy", cal).toString()
         }
 
-//        fun loadPdfSize(pdfUrl: String?, pdfTitle: String){
-//            val TAG = "PDF_SIZE_TAG"
-//
-//            val ref = FirebaseStorage.getInstance().getReferenceFromUrl(pdfUrl)
-//            ref.metadata
-//                .addOnSuccessListener {
-//
-//                }
-//                .addOnFailureListener {
-//
-//                }
-//        }
+        fun loadPdfSize(pdfUrl: String, pdfTitle: String, sizeTv: TextView){
+
+            val ref = FirebaseStorage.getInstance().getReferenceFromUrl(pdfUrl)
+            ref.metadata
+                .addOnSuccessListener { storageMetadata->
+                    val bytes = storageMetadata.sizeBytes.toDouble()
+                    val kb = bytes/1024
+                    val mb = kb/1024
+                    if(mb>=1){
+                        sizeTv.text = "${String.format("$.2f", mb)} MB"
+                    } else if (kb >= 1){
+                        sizeTv.text = "${String.format("$.2f", kb)} KB"
+                    } else {
+                        sizeTv.text = "${String.format("$.2f", bytes)} bytes"
+                    }
+                }
+                .addOnFailureListener {
+
+                }
+        }
 
         fun loadPdfFromUrlSinglePage(
             pdfUrl: String,
@@ -75,8 +88,26 @@ class MyApplication : Application() {
                     Log.e("PDF_LOAD_ERROR", "Failed to load PDF: ${exception.message}")
                 }
         }
+        fun loadCategory(categoryId: String, categoryTv: TextView){
+            val ref = FirebaseDatabase.getInstance().getReference("Categories")
+            ref.child(categoryId)
+                .addListenerForSingleValueEvent(object: ValueEventListener{
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        val category: String = "${snapshot.child("category").value}"
 
+                        // set
+                        categoryTv.text = category
+                    }
+
+                    override fun onCancelled(error: DatabaseError) {
+                        TODO("Not yet implemented")
+                    }
+
+                })
+        }
 
     }
+
+
 
 }
