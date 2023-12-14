6046ccfa8c985e735db982f37c1656edb37ec892
Lancelot <bulanlancelot@gmail.com>
2023-12-14 16:35:06 +0800

    Add folder for unused files (for now)

--- app/src/main/java/com/example/starboundlibrary/unused/Favorite.kt
+++ app/src/main/java/com/example/starboundlibrary/unused/Favorite.kt
@@ -1,21 +1,21 @@
-package com.example.starboundlibrary
+package com.example.starboundlibrary.unused
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.starboundlibrary.R
 
-class Home : Fragment() {
+
+class Favorite : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_favorite, container, false)
     }
-
-
 
 }
--- app/src/main/java/com/example/starboundlibrary/unused/Profile.kt
+++ app/src/main/java/com/example/starboundlibrary/unused/Profile.kt
@@ -1,20 +1,20 @@
-package com.example.starboundlibrary
+package com.example.starboundlibrary.unused
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.starboundlibrary.R
 
 
-class Favorite : Fragment() {
-
+class Profile : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite, container, false)
+        return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
 }
